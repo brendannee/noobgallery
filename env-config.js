77e88bf38e8729bf1b876682ec2885f71e80d504
Brendan Nee <brendan@blinktag.com>
2018-07-02 00:08:50 -0700

    Use img.wanderingnoobs.com

--- env-config.js
+++ env-config.js
@@ -1,6 +1,6 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.GALLERY_URL': 'http://img.wanderingnoobs.com.s3-website-us-east-1.amazonaws.com',
+  'process.env.GALLERY_URL': 'http://img.wanderingnoobs.com',
   'process.env.GALLERY_TITLE': 'Wandering Noobs'
 }
