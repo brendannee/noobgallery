deb5925237d00bb748c7b4addfb54d678f65b930
Brendan Nee <brendan@blinktag.com>
2018-07-06 11:49:57 -0700

    Handle missing gallery

--- pages/index.js
+++ pages/index.js
@@ -23,42 +23,57 @@ export default class Index extends React.Component {
     const masonry = require('masonry-layout')
   }
 
-  render() {
+  renderGallery() {
+    if (!this.props.galleries || !this.props.galleries.length) {
+      return (
+        <div className="warning">
+          <h3>No Galleries Found</h3>
+        </div>
+      )
+    }
+
     const thumbnailWidth = 400
+
+    return (
+      <div
+        id="lightgallery"
+        className="grid gallery gallery-index"
+        data-masonry={`{ "itemSelector": ".grid-item", "columnWidth": ${thumbnailWidth}, "gutter": 10 }`}
+      >
+        {this.props.galleries.map((gallery, key) => {
+          const height = Math.round(thumbnailWidth / gallery.imageSize.width * gallery.imageSize.height);
+          return (
+            <div
+              className="grid-item"
+              key={key}
+              style={{width: `${thumbnailWidth}px`, height: `${height + 40}px`}}
+            >
+              <Link href={`/gallery/${gallery.galleryId}/`} >
+                <a className="photo-link">
+                  <img
+                    src={`${process.env.GALLERY_URL}/${gallery.medium}`}
+                    width={thumbnailWidth}
+                    height={height}
+                  />
+                </a>
+              </Link>
+              <Link href={`/gallery/${gallery.galleryId}/`} >
+                <a className="photo-link-title">{_.startCase(gallery.galleryId)}</a>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  render() {
     return (
       <Layout>
         <div className="gallery-wrapper">
           <h1 className="gallery-title">{process.env.GALLERY_TITLE}</h1>
           <div className="gallery-description">{process.env.GALLERY_DESCRIPTION}</div>
-          <div
-            id="lightgallery"
-            className="grid gallery gallery-index"
-            data-masonry={`{ "itemSelector": ".grid-item", "columnWidth": ${thumbnailWidth}, "gutter": 10 }`}
-          >
-            {this.props.galleries.map((gallery, key) => {
-              const height = Math.round(thumbnailWidth / gallery.imageSize.width * gallery.imageSize.height);
-              return (
-                <div
-                  className="grid-item"
-                  key={key}
-                  style={{width: `${thumbnailWidth}px`, height: `${height + 40}px`}}
-                >
-                  <Link href={`/gallery/${gallery.galleryId}/`} >
-                    <a className="photo-link">
-                      <img
-                        src={`${process.env.GALLERY_URL}/${gallery.medium}`}
-                        width={thumbnailWidth}
-                        height={height}
-                      />
-                    </a>
-                  </Link>
-                  <Link href={`/gallery/${gallery.galleryId}/`} >
-                    <a className="photo-link-title">{_.startCase(gallery.galleryId)}</a>
-                  </Link>
-                </div>
-              )
-            })}
-          </div>
+          {this.renderGallery()}
         </div>
       </Layout>
     )
